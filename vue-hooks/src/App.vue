4900f9b9b935ec9a036db30140beb9b25431896d
John Maybee <john.d.maybee@gmail.com>
2023-06-01 15:12:18 -0400

    refactor: installed a vue router and reconfigured the app to have a router that is imported into main.js through the routes

--- vue-hooks/src/App.vue
+++ vue-hooks/src/App.vue
@@ -1,34 +1,34 @@
 <script>
-import HomePage from "./components/HomePage.vue";
-import LoginPage from "./components/LoginPage.vue";
-import UserPage from "./components/UserPage.vue";
+// import HomePage from "./views/HomePage.vue";
+// import LoginPage from "./views/LoginPage.vue";
+// import UserPage from "./views/UserPage.vue";
 
-export default {
-  components: {
-    HomePage,
-    LoginPage,
-    UserPage,
-  },
-  data: () => ({
-    currentPage: "User",
-  }),
-  computed: {
-    renderPage() {
-      return this.currentPage + "Page";
-    }
-  },
-  methods: {
-    showHomePage() {
-      this.currentPage = "Home";
-    },
-    showLoginPage() {
-      this.currentPage = "Login";
-    },
-    showUserPage() {
-      this.currentPage = "User";
-    },
-  },
-};
+// export default {
+//   components: {
+//     HomePage,
+//     LoginPage,
+//     UserPage,
+//   },
+//   data: () => ({
+//     currentPage: "User",
+//   }),
+//   computed: {
+//     renderPage() {
+//       return this.currentPage + "Page";
+//     }
+//   },
+//   methods: {
+//     showHomePage() {
+//       this.currentPage = "Home";
+//     },
+//     showLoginPage() {
+//       this.currentPage = "Login";
+//     },
+//     showUserPage() {
+//       this.currentPage = "User";
+//     },
+//   },
+// };
 </script>
 
 <template>
@@ -37,16 +37,20 @@ export default {
       <img src="@/assets/vue-heart.png" width="30" />C'est La Vue
     </span>
     <nav class="nav">
-      <a href="#" @click.prevent="showHomePage">Home</a>
+      <router-link to="/">Home</router-link>
+      <router-link to="/login">Login</router-link>
+      <router-link to="/user">User</router-link>
+      <!-- <a href="#" @click.prevent="showHomePage">Home</a>
       <a href="#" @click.prevent="showLoginPage">Login</a>
-      <a href="#" @click.prevent="showUserPage">User</a>
+      <a href="#" @click.prevent="showUserPage">User</a> -->
     </nav>
   </header>
 
   <!-- <HomePage v-if="currentPage === 'Home'" />
   <LoginPage v-else /> -->
   <Suspense>
-    <component :is="renderPage" :key="renderPage" />
+    <router-view />
+    <!-- <component :is="renderPage" :key="renderPage" /> -->
 
     <template v-slot:fallback> Data is loading... </template> 
   </Suspense>
--- vue-hooks/src/main.js
+++ vue-hooks/src/main.js
@@ -1,4 +1,28 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes: [
+        {
+            path: '/',
+            component: () => import('@/views/HomePage.vue'),
+        },
+        {
+            path: '/login',
+            component: () => import('@/views/LoginPage.vue'),
+
+        },
+        {
+            path: '/user',
+            component: () => import('@/views/UserPage.vue'),
+
+        },
+    ]
+})
+app.use(router)
+app.mount('#app')
