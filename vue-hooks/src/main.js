54f8315d2626c79f031023239150a07e572f0af8
John Maybee <john.d.maybee@gmail.com>
2023-06-01 19:10:08 -0400

    imported createPinia into main.js then had the app use pinia

--- vue-hooks/src/main.js
+++ vue-hooks/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHashHistory } from 'vue-router'
+import { createPinia } from 'pinia'
 
 
 import App from './App.vue'
@@ -29,5 +30,9 @@ const router = createRouter({
 
     ]
 })
+
+const pinia = createPinia()
+
 app.use(router)
+app.use(pinia)
 app.mount('#app')
